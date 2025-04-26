8b9a500cd0a4eddf46e5e1ff279eeb69874ff433
Rocco <13131155@me.mcu.edu.tw>
2025-04-26 15:43:08 +0800

    feat(nav): 新增導覽列元件實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ import { Product } from './models/product';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
   products: Product[] = [
     new Product({
       id: 1,
